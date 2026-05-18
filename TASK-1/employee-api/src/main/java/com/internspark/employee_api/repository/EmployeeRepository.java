bb221867b239a4c0432839b2f5a6dff5e36841c0
THRIPURARIBHATLA V N S APARNA <tvnsaparna@gmail.com>
2026-05-18 12:02:51 +0530

    Add files via upload
    Task 1: RESTful Employee API with Spring Boot

---
+++ TASK-1/employee-api/src/main/java/com/internspark/employee_api/repository/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.internspark.employee_api.repository;
+
+import com.internspark.employee_api.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
